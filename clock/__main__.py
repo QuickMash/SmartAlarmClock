a035a6bf7cd6185e8208ef8fe903984cb8e0be78
QuickMash <106212829+QuickMash@users.noreply.github.com>
2025-01-21 09:48:05 -0700

    Update __main__.py

--- clock/__main__.py
+++ clock/__main__.py
@@ -1,3 +1,18 @@
 import requests
 import time
 import datetime
+import flask
+import os
+import json
+
+
+def main():
+  time = json.loads(requests.get("http://worldtimeapi.org/api"))
+  print(time)
+  print("Formatting...")
+  print(time["time"])
+  
+if __name__ == "__main__":
+  main()
+else:
+  sys.exit(f"{__name__} must be ran directly")
